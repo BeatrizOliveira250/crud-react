98e4a6ff1380851587e7e21b90d438ea42b0849a
Beatriz Oliveira <anabeatrizxoliveira@gmail.com>
2021-02-16 00:01:56 -0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import Panel from './pages/Panel';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+        <Switch>
+          <Route path="/sign-in" component={SignIn} />
+          <Route path="/" component={Panel} />
+        </Switch>
+    </BrowserRouter>
   );
 }
 
